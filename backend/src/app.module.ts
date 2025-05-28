c8dcb7df3e68405b22db12ab370c94fdf18f4e0a
Khaled <khaledesam384@gmail.com>
2025-05-28 17:02:12 +0300

    chore(app): register auth, user, and group modules in root app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,32 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+
+import { MessageModule } from './message/message.module';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      logging: true,// logs SQL queries for debugging
+      synchronize: true,// WARNING: Disable in production
+    }),
+    MessageModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
